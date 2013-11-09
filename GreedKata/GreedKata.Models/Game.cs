54ae8333aa756b967c21f8ac8598ed0f41e64b74
Mark Baker <shakybaker@gmail.com>
2013-11-09 18:28:01 +0000

    all tests

---
+++ GreedKata/GreedKata.Models/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedKata.Models
+{
+    public class Game : IGame
+    {
+        private bool isStraight = false;
+
+        public int Score(int[] dice)
+        {
+            int score = 0;
+
+            score += GetStraightScore(dice);
+
+            if (!isStraight)
+            {
+                if (dice.Where(x => x.Equals(1)).Count() == 1)
+                    score += 100;
+
+                if (dice.Where(x => x.Equals(5)).Count() == 1)
+                    score += 50;
+
+                for (var i = 1; i <= 6; i++)
+                    score += GetMultipleScore(dice, i);
+
+                score += GetThreePairScore(dice);
+            }
+    
+            return score;
+        }
+
+        private int GetStraightScore(int[] dice)
+        {
+            int retVal = 0;
+
+            if ((dice.Where(x => x.Equals(1)).Count() == 1)
+            && (dice.Where(x => x.Equals(2)).Count() == 1)
+            && (dice.Where(x => x.Equals(3)).Count() == 1)
+            && (dice.Where(x => x.Equals(4)).Count() == 1)
+            && (dice.Where(x => x.Equals(5)).Count() == 1)
+            && (dice.Where(x => x.Equals(6)).Count() == 1))
+            {
+                retVal = 1200;
+                isStraight = true;
+            }
+
+            return retVal;
+        }
+
+        private int GetThreePairScore(int[] dice)
+        {
+            int retVal = 0;
+            int count = 0;
+
+            for (var i = 1; i <= 6; i++)
+                if (dice.Where(x => x.Equals(i)).Count() == 2)
+                    count++;
+
+            if (count == 3)
+                retVal = 800;
+
+            return retVal;
+        }
+
+        private static int GetMultipleScore(int[] dice, int check)
+        {
+            int retVal = 0;
+            int multiScore = 0;
+
+            switch (check)
+            {
+                case 1:
+                    multiScore = 1000;
+                    break;
+                case 2:
+                    multiScore = 200;
+                    break;
+                case 3:
+                    multiScore = 300;
+                    break;
+                case 4:
+                    multiScore = 400;
+                    break;
+                case 5:
+                    multiScore = 500;
+                    break;
+                case 6:
+                    multiScore = 600;
+                    break;
+                default:
+                    break;
+            }
+
+            if (dice.Where(x => x.Equals(check)).Count() == 6)
+                retVal += multiScore * 8;
+            else if (dice.Where(x => x.Equals(check)).Count() == 5)
+                retVal += multiScore * 4;
+            else if (dice.Where(x => x.Equals(check)).Count() == 4)
+                retVal += multiScore * 2;
+            else if (dice.Where(x => x.Equals(check)).Count() == 3)
+                retVal += multiScore;
+
+            return retVal;
+        }
+    }
+}
---
+++ GreedKata/GreedKata.Tests/UnitTests.cs
@@ -0,0 +1,146 @@
+﻿using GreedKata.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedKata.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        //see http://nimblepros.com/media/36619/greed%20kata.pdf for game rules
+
+        [Test]
+        public void Player_rolls_a_single_1()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 1, 2, 3, 4, 6 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(100, actual);
+        }
+
+        [Test]
+        public void Player_rolls_a_single_5()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 6, 2, 3, 4, 5 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(50, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_1s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 1, 1, 1, 4, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(1000, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_2s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 2, 2, 4, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(200, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_3s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 3, 3, 4, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(300, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_4s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 4, 4, 4, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(400, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_5s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 5, 5, 5, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(500, actual);
+        }
+
+        [Test]
+        public void Player_rolls_triple_6s()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 6, 6, 6, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(600, actual);
+        }
+
+        [Test]
+        public void Player_rolls_4_of_a_kind()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 6, 6, 6, 6, 3 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(1200, actual);
+        }
+
+        [Test]
+        public void Player_rolls_5_of_a_kind()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 6, 6, 6, 6, 6 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(2400, actual);
+        }
+
+        [Test]
+        public void Player_rolls_6_of_a_kind()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 6, 6, 6, 6, 6, 6 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(4800, actual);
+        }
+
+        [Test]
+        public void Player_rolls_3_pairs()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 2, 2, 3, 3, 4, 4 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(800, actual);
+        }
+
+        [Test]
+        public void Player_rolls_a_straight()
+        {
+            IGame game = new Game();
+            var dice = new int[] { 1, 2, 3, 4, 5, 6 };
+            var actual = game.Score(dice);
+
+            Assert.AreEqual(1200, actual);
+        }
+    }
+}
